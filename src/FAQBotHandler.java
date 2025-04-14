513ea23c15d723465a7e32152b9ef4a7ee00dc34
Kristina <tolstovachristina@gmail.com>
2025-04-15 02:39:58 +0500

    Update FAQBotHandler.java

--- src/FAQBotHandler.java
+++ src/FAQBotHandler.java
@@ -1,6 +1,15 @@
 public class FAQBotHandler extends SupportHandler{
     @Override
     public void handle(String issue) {
-        
+        if (issue.equals("password_reset")) {
+            System.out.println("FAQBot! Handled password_reset");
+        } else {
+            log("FAQBot", issue);
+            if (next != null) next.handle(issue);
+        }
+    }
+
+    private void log(String handler, String issue) {
+        System.out.println("Logger! " + handler + " tried to handle " + issue);
     }
 }

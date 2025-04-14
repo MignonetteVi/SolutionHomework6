b04f21112389f8f87af102180fae94627ee3a5e8
Kristina <tolstovachristina@gmail.com>
2025-04-15 02:23:41 +0500

    Update SupportHandler.java

--- src/SupportHandler.java
+++ src/SupportHandler.java
@@ -1,2 +1,10 @@
-public class SupportHandler {
+abstract class SupportHandler {
+    protected SupportHandler next;
+
+    public SupportHandler setNext(SupportHandler handler) {
+        this.next = handler;
+        return handler;
+    }
+
+    public abstract void handle(String issue);
 }

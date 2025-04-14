d8cfcde3a480bb1826ff45c15b2b9a4afacde66c
Kristina <tolstovachristina@gmail.com>
2025-04-15 02:47:09 +0500

    Update JuniorSupportHandler.java

--- src/JuniorSupportHandler.java
+++ src/JuniorSupportHandler.java
@@ -1,6 +1,15 @@
 public class JuniorSupportHandler extends SupportHandler{
     @Override
     public void handle(String issue) {
-        
+        if (issue.equals("refund_request") || issue.equals("billing_issue")) {
+            System.out.println("JuniorSupport! Handled " + issue);
+        } else {
+            log("JuniorSupport", issue);
+            if (next != null) next.handle(issue);
+        }
+    }
+
+    private void log(String handler, String issue) {
+        System.out.println("Logger! " + handler + " tried to handle " + issue);
     }
 }

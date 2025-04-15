90219269175baad47cc45fa45b2297971b30fb11
Kristina <tolstovachristina@gmail.com>
2025-04-15 20:54:51 +0500

    Update TurnOnLightCommand.java

--- src/TurnOnLightCommand.java
+++ src/TurnOnLightCommand.java
@@ -1,11 +1,20 @@
 public class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    public TurnOnLightCommand() {
+    }
+
     @Override
     public void execute() {
-        
+        light.turnOn();
     }
 
     @Override
     public void undo() {
-
+        light.turnOff();
     }
 }

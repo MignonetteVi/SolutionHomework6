2a66671d514b51b9533b3c313919b95ac15274f3
Kristina <tolstovachristina@gmail.com>
2025-04-15 21:20:46 +0500

    Update SmartHomeRemoteControl.java

--- src/SmartHomeRemoteControl.java
+++ src/SmartHomeRemoteControl.java
@@ -1,2 +1,23 @@
 public class SmartHomeRemoteControl {
+    private java.util.Map<String, Command> commandSlots = new java.util.HashMap<>();
+    private java.util.Stack<Command> history = new java.util.Stack<>();
+
+    public void setCommand(String slot, Command command) {
+        commandSlots.put(slot, command);
+    }
+
+    public void pressButton(String slot) {
+        Command command = commandSlots.get(slot);
+        if (command != null) {
+            command.execute();
+            history.push(command);
+        }
+    }
+
+    public void undoButton() {
+        if (!history.isEmpty()) {
+            Command command = history.pop();
+            command.undo();
+        }
+    }
 }

307c450c6a7fb574ed4837f190d24b96c415f238
Kristina <tolstovachristina@gmail.com>
2025-04-15 20:50:56 +0500

    Update Thermostat.java

--- src/Thermostat.java
+++ src/Thermostat.java
@@ -1,2 +1,15 @@
 public class Thermostat {
+    private int temperature;
+    private int prevTemperature;
+
+    public void setTemperature(int temperature) {
+        prevTemperature = this.temperature;
+        this.temperature = temperature;
+        System.out.println("Thermostat! Setting temperature to " + temperature + "°C");
+    }
+
+    public void revert() {
+        temperature = prevTemperature;
+        System.out.println("Thermostat! Reverting to previous temperature");
+    }
 }
